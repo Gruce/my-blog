018ca92e2d56125048ed5a02c73ba96ca9c2907e
Hassan Alkhalidi <gruceing@gmail.com>
2022-06-07 23:13:19 +0300

    asd

--- components/content/GithubProjects.vue
+++ components/content/GithubProjects.vue
@@ -14,11 +14,9 @@ const { data, pending, error, refresh } = await useFetch(
     <ul v-if="data" class="p-0">
       <li v-for="(item, index) in data" :key="index" class="flex space-x-2 group">
         <span class="flex-shrink-0 w-6 h-6 text-gray-300">{{index+1}}</span>
-        <span>
+        <a target="_blank" :href="item.html_url" class="no-underline hover:underline">
           <Markdown :use="() => item.name" unwrap="li" />
-        </span>
-        <a class="invisible ml-2 group-hover:visible" :href="item.html_url">
-            <Icon name="ph:link-simple-bold" />
+          <Icon name="ph:link-simple-bold" class="invisible ml-1 group-hover:visible" />
         </a>
         <span class="mx-2 text-gray-500"> — {{new Date(item.created_at).toDateString()}}</span>
       </li>
